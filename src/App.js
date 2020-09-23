74a7270f76b4018b38806aaf0cf4b7c75a2b2341
Sahan Reddy <sahan.reddy.58@gmail.com>
2020-09-23 15:09:59 -0400

    yeet

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import USAMap from "react-usa-map";
 
-class App extends Component {
-  mapHandler = (event) => {
-    alert(event.target.dataset.name);
+function App() {
+  const [mapConfig, setMapConfig] = useState({});
+
+  // next click event should reset map
+  const [mapReset, setMapReset] = useState(false);
+  
+
+  const mapHandler = (event) => {
+    const state = event.target.dataset.name;
+
+    const config = {}
+    config[state] = { fill: "navy" };
+
+    console.log(config);
+    setMapConfig(config);
   };
 
-  statesFilling = () => {
-    return {
-      "NJ": {
-        fill: "navy",
-        clickHandler: () => alert("Custom callback for the NJ state")
-      },
-      "NY": {
-        fill: "#CC0000"
-      }
-    };
-  };
 
-  render() {
-    return (
-      <div className="App">
-        <h1>react-usa-map</h1>
-        <USAMap customize={this.statesFilling()} onClick={this.mapHandler} />
-        <p>More information: <a href="http://github.com/gabidavila/react-usa-map">GitHub</a></p>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>react-usa-map</h1>
+      <USAMap customize={mapConfig} onClick={mapHandler} />
+      <p>More information: <a href="http://github.com/gabidavila/react-usa-map">GitHub</a></p>
+    </div>
+  );
 }
 
 export default App;
